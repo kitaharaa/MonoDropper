c64f9ff9a55d52faa786f5e4d0b8d9f10b363144
amogus3000 <bvalentin1415@gmail.com>
2023-10-16 11:27:40 +0300

    added card ui

--- app/src/main/java/com/kitaharaa/monodropper/ui/base/BaseScreen.kt
+++ app/src/main/java/com/kitaharaa/monodropper/ui/base/BaseScreen.kt
@@ -5,8 +5,12 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
@@ -18,11 +22,16 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.kitaharaa.monodropper.model.card.CardType
 import com.kitaharaa.monodropper.model.card.UserCard
+import com.kitaharaa.monodropper.ui.theme.Income
+import com.kitaharaa.monodropper.ui.theme.Outcome
 
 val cardList = listOf(
     UserCard(
@@ -90,7 +99,6 @@ fun UserCardView(cardInfo: UserCard, width: Dp) {
     ElevatedCard(
         modifier = Modifier
             .width(width)
-            .height(250.dp)
             .background(
                 when (cardInfo.cardType) {
                     CardType.Black -> Color.Black
@@ -98,9 +106,52 @@ fun UserCardView(cardInfo: UserCard, width: Dp) {
                 }
             )
     ) {
+        Spacer(Modifier.size(40.dp))
+
         Text(
-            modifier = Modifier.padding(15.dp), text = cardInfo.userName
+            modifier = Modifier.padding(horizontal = 20.dp),
+            text = cardInfo.userName,
+            fontSize = 22.sp,
+            fontWeight = FontWeight.Bold
         )
+
+        Spacer(Modifier.size(30.dp))
+
+        Text(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(
+                    start = 30.dp,
+                    end = 15.dp
+                ),
+            text = cardInfo.cartNumber,
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold,
+            textAlign = TextAlign.End
+        )
+
+        Spacer(Modifier.size(30.dp))
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(end = 10.dp),
+            horizontalArrangement = Arrangement.End
+        ) {
+            Text(
+                text = "+${cardInfo.monthReplenishment}",
+                color = Income
+            )
+
+            Spacer(modifier = Modifier.width(15.dp))
+
+            Text(
+                text = "-${cardInfo.monthDebit}",
+                color = Outcome
+            )
+        }
+
+        Spacer(Modifier.size(40.dp))
     }
 }
 
